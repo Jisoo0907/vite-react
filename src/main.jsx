2c31b3a57313c354a7ff64c7863ee98d0bc2b73d
jisoolee <szjslee@gmail.com>
2024-07-31 18:26:50 +0900

    prettier 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import "@/styles/globals.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "/App";
-import "@/styles/globals.css";
 
 const domNode = document.getElementById("react-app");
 
